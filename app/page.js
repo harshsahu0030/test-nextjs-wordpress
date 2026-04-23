f7fec1824e0a7b660a770430b70345b1067901a6
harshsahu0030 <harsh0030sahu@gmail.com>
2026-04-23 16:58:16 +0530

    ready to deloy

--- app/page.js
+++ app/page.js
@@ -12,18 +12,20 @@ export async function getTreatments() {
     return await res.json();
   } catch (e) {
     console.error(e);
-
     return null;
   }
 }
 
 const page = async () => {
   const treatments = await getTreatments();
+  console.log(treatments);
+
   return (
     <div className="min-h-screen w-full bg-black text-white flex items-center justify-center flex-col gap-10">
+      <h1 className="text-center text-5xl">Treatments</h1>
       {treatments?.map((item) => (
         <div key={item.id} className="p-4 border border-white rounded">
-          <h2 className="text-xl font-bold mb-2">{item.title.rendered}</h2>
+          <h2 className="text-base font-bold mb-2">{item.title.rendered}</h2>
           <div
             className="text-sm"
             dangerouslySetInnerHTML={{ __html: item.content.rendered }}
